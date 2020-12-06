7e2be6205b6072c6830f48b0f887c7e19547eaca
MMirbach <m.mirbach153@gmail.com>
2020-12-06 12:34:37 +0200

    removed timer

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 #!python3
 from sys import argv
 import simulator
-from datetime import datetime
-
-start = datetime.now()
 
 stay_probabilities = [float(probability) for probability in argv[5:]]
 simulator.init(float(argv[1]), float(argv[2]), float(argv[3]), float(argv[4]), stay_probabilities)
 simulator.run()
 simulator.print_results()
-
-print(datetime.now() - start)
