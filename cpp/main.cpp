8f5d729d1c20c87ae936762920c30eca930775c7
MMirbach <m.mirbach153@gmail.com>
2020-12-07 16:31:03 +0200

    make works

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,11 @@
-#include "ClinicSimulator.h"
+#include "clinicSimulator.h"
 #include <cstdlib>
 #include <vector>
 
 using std::strtod;
 
 int main(int argc, char* argv[]) {
-	std::vector<double> stay_probabilities;
+    std::vector<double> stay_probabilities;
 	for (int i = 5; i < argc; i++) stay_probabilities.push_back(strtod(argv[i], nullptr));
 	ClinicSimulator* sim = sim->getInstance(strtod(argv[1], nullptr), strtol(argv[2], nullptr, 10), 
 		strtod(argv[3], nullptr), strtod(argv[4], nullptr), stay_probabilities);
